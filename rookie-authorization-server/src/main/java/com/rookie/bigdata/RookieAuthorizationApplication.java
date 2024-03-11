201a30153539450a82b8a51f15e1650df3fa40ef
albert.liu435 <albert.liu435@gmail.com>
2024-03-11 20:55:11 +0800

    init

---
+++ rookie-authorization-server/src/main/java/com/rookie/bigdata/RookieAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.rookie.bigdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RookieAuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RookieAuthorizationApplication.class, args);
+    }
+
+}
