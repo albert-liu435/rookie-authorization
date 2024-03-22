0704361e123610005cc390785738e9f2079a26a3
liuxili01 <liuxili@bestseller.com.cn>
2024-03-22 14:12:47 +0800

    Spring Authorization Server入门 (九) Spring Boot引入Resource Server对接认证服务

--- rookie-authorization-servers/rookie-authorization-server/src/main/java/com/rookie/bigdata/config/AuthorizationConfig.java
+++ rookie-authorization-servers/rookie-authorization-server/src/main/java/com/rookie/bigdata/config/AuthorizationConfig.java
@@ -422,7 +422,13 @@ public class AuthorizationConfig {
      */
     @Bean
     public AuthorizationServerSettings authorizationServerSettings() {
-        return AuthorizationServerSettings.builder().build();
+        return AuthorizationServerSettings.builder()
+                /*
+                    设置token签发地址(http(s)://{ip}:{port}/context-path, http(s)://domain.com/context-path)
+                    如果需要通过ip访问这里就是ip，如果是有域名映射就填域名，通过什么方式访问该服务这里就填什么
+                 */
+                .issuer("http://192.168.80.144:8080")
+                .build();
     }
 
     /**
@@ -442,5 +448,8 @@ public class AuthorizationConfig {
                 .build();
         return new InMemoryUserDetailsManager(user);
     }
+
+
+
 
 }
