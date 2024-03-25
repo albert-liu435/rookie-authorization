cfa518b87f29c6b6b27042968178c353ae87dc5f
liuxili01 <liuxili@bestseller.com.cn>
2024-03-25 16:25:31 +0800

    Spring Authorization Server优化篇：添加Redis缓存支持和统一响应类

--- rookie-authorization-servers/rookie-authorization-server/src/main/java/com/rookie/bigdata/controller/AuthorizationController.java
+++ rookie-authorization-servers/rookie-authorization-server/src/main/java/com/rookie/bigdata/controller/AuthorizationController.java
@@ -15,6 +15,7 @@ import org.springframework.security.oauth2.server.authorization.OAuth2Authorizat
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.WebAttributes;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -45,41 +46,14 @@ public class AuthorizationController {
 
     private final OAuth2AuthorizationConsentService authorizationConsentService;
 
-
-
     @ResponseBody
-    @GetMapping("/getSmsCaptcha")
-    public Map<String,Object> getSmsCaptcha(String phone, HttpSession session) {
-        // 这里应该返回一个统一响应类，暂时使用map代替
-        Map<String,Object> result = new HashMap<>();
-        result.put("code", HttpStatus.OK.value());
-        result.put("success", true);
-        result.put("message", "获取短信验证码成功.");
-        // 固定1234
-        result.put("data", "1234");
-        // 存入session中
-        session.setAttribute(phone, "1234");
-        return result;
+    @GetMapping("/user")
+    public Map<String, Object> user(Principal principal) {
+        if (!(principal instanceof JwtAuthenticationToken token)) {
+            return Collections.emptyMap();
+        }
+        return token.getToken().getClaims();
     }
-
-
-    @ResponseBody
-    @GetMapping("/getCaptcha")
-    public Map<String,Object> getCaptcha(HttpSession session) {
-        // 使用hutool-captcha生成图形验证码
-        // 定义图形验证码的长、宽、验证码字符数、干扰线宽度
-        ShearCaptcha captcha = CaptchaUtil.createShearCaptcha(150, 40, 4, 2);
-        // 这里应该返回一个统一响应类，暂时使用map代替
-        Map<String,Object> result = new HashMap<>();
-        result.put("code", HttpStatus.OK.value());
-        result.put("success", true);
-        result.put("message", "获取验证码成功.");
-        result.put("data", captcha.getImageBase64Data());
-        // 存入session中
-        session.setAttribute("captcha", captcha.getCode());
-        return result;
-    }
-
 
     @GetMapping("/activate")
     public String activate(@RequestParam(value = "user_code", required = false) String userCode) {
@@ -169,6 +143,7 @@ public class AuthorizationController {
     public static class ScopeWithDescription {
         private static final String DEFAULT_DESCRIPTION = "UNKNOWN SCOPE - We cannot provide information about this permission, use caution when granting this.";
         private static final Map<String, String> scopeDescriptions = new HashMap<>();
+
         static {
             scopeDescriptions.put(
                     OidcScopes.PROFILE,
