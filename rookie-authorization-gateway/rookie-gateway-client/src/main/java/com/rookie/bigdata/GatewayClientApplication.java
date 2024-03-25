f1f80731cd938897af39bd58df830e7ba49f4941
albert.liu435 <albert.liu435@gmail.com>
2024-03-25 23:17:59 +0800

    修复低版本snakeyaml漏洞，添加网关对接OAuth2认证服务示例 202308261919

---
+++ rookie-authorization-gateway/rookie-gateway-client/src/main/java/com/rookie/bigdata/GatewayClientApplication.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 网关启动类
+ *
+ * @author vains
+ */
+@SpringBootApplication
+public class GatewayClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayClientApplication.class, args);
+    }
+
+}
---
+++ rookie-authorization-gateway/rookie-gateway-resource/src/main/java/com/rookie/bigdata/ResourceNormalApplication.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 资源服务启动类
+ *
+ * @author vains
+ */
+@SpringBootApplication
+public class ResourceNormalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceNormalApplication.class, args);
+    }
+
+}
